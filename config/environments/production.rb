fa5b7f69f62f35af20301e3f8556e1becbcd9e05
Andrew Lott <AndrewLott@gmail.com>
2017-01-26 02:29:57 -0800

    hopefully fixing serving issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# Serve Static files
+config.serve_static_files = true
